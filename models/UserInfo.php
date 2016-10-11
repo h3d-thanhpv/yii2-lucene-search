818cd1dcf924c318fc31203f30cfe9e36be85b89
Phan Van Thanh <thanhpv@projectkit.net>
2016-10-11 14:33:27 +0700

    Add behavior for lucene search in model :penguin:

--- models/UserInfo.php
+++ models/UserInfo.php
@@ -2,8 +2,34 @@
 
 namespace app\models;
 
+use himiklab\yii2\search\behaviors\SearchBehavior;
 
 class UserInfo extends base\UserInfo
 {
-
+    public function behaviors()
+    {
+        return array_merge(parent::behaviors(), [
+            'search' => [
+                'class' => SearchBehavior::className(),
+                'searchScope' => function ($model) {
+                    /** @var \yii\db\ActiveQuery $model */
+                    $model->select('*');
+                },
+                'searchFields' => function ($model) {
+                    /** @var self $model */
+                    return [
+                        ['name' => 'ID', 'value' => $model->id, 'type' => SearchBehavior::FIELD_UNINDEXED],
+                        ['name' => 'USER_NAME', 'value' => $model->user_name, 'type' => SearchBehavior::FIELD_KEYWORD],
+                        ['name' => 'FIRST_NAME', 'value' => $model->first_name, 'type' => SearchBehavior::FIELD_KEYWORD],
+                        ['name' => 'LAST_NAME', 'value' => $model->last_name, 'type' => SearchBehavior::FIELD_KEYWORD],
+                        ['name' => 'EMAIL', 'value' => $model->email, 'type' => SearchBehavior::FIELD_KEYWORD],
+                        ['name' => 'GENDER', 'value' => $model->gender, 'type' => SearchBehavior::FIELD_KEYWORD],
+                        ['name' => 'CREATE_DATE', 'value' => $model->create_date, 'type' => SearchBehavior::FIELD_KEYWORD],
+                        ['name' => 'COMPANY', 'value' => $model->company, 'type' => SearchBehavior::FIELD_TEXT],
+                        ['name' => 'USER_AGENT', 'value' => $model->user_agent, 'type' => SearchBehavior::FIELD_TEXT],
+                    ];
+                }
+            ],
+        ]);
+    }
 }
