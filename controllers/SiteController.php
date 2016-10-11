4fb1b487d0cabaa8bddd94eeab4e50fc05bd6262
Phan Van Thanh <thanhpv@projectkit.net>
2016-10-11 16:47:26 +0700

    Update search and view :turtle:

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -2,12 +2,18 @@
 
 namespace app\controllers;
 
+use app\models\UserInfo;
+use app\models\UserInfoSearch;
+use himiklab\yii2\search\Search;
 use Yii;
+use yii\data\ActiveDataProvider;
 use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use ZendSearch\Lucene\Analysis\Analyzer\Analyzer;
+use ZendSearch\Lucene\Analysis\Analyzer\Common\Utf8Num\CaseInsensitive;
 
 class SiteController extends Controller
 {
@@ -58,9 +64,32 @@ class SiteController extends Controller
      *
      * @return string
      */
-    public function actionIndex()
+    public function actionIndex($q = "")
     {
-        return $this->render('index');
+        if($q != "") {
+            ini_set('memory_limit', '-1');
+            /** @var Search $search */
+            $search = Yii::$app->search;
+            Analyzer::setDefault(new CaseInsensitive());
+            /** @var array $searchData */
+            $searchData = $search->find(mb_strtoupper($q, 'UTF-8'));
+            if(is_array($searchData) && array_key_exists('results', $searchData)) {
+                $userInfoIds = array_map(function($p) { return $p->ID; }, $searchData['results']);
+            } else {
+                $userInfoIds = [];
+            }
+            $dataProvider = new ActiveDataProvider([
+                'query' => UserInfo::find()->where(['id' => $userInfoIds]),
+            ]);
+        } else {
+            $dataProvider = new ActiveDataProvider([
+                'query' => UserInfo::find(),
+            ]);
+        }
+        return $this->render('index', [
+            'query' => $q,
+            'dataProvider' => $dataProvider,
+        ]);
     }
 
     /**
--- views/site/index.php
+++ views/site/index.php
@@ -1,53 +1,48 @@
 <?php
 
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
 /* @var $this yii\web\View */
+/* @var $searchModel app\models\UserInfoSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'My Yii Application';
+$this->title = Yii::t('app', 'User Infos');
+$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-index">
+<div class="user-info-index">
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
+    <h1><?= Html::encode($this->title) ?></h1>
+    <div class="row">
+        <div class="col-lg-6 col-lg-offset-6 col-md-6 col-md-offset-6 col-sm-7 col-sm-offset-5 col-xs-12">
+            <?= Html::beginForm(\yii\helpers\Url::to(['site/index']), 'get', ['class' => 'form-inline']) ?>
+            <div class="input-group pull-right col-lg-10 col-md-11 col-sm-12">
+                <?= Html::textInput('q', $query, ['class' => 'form-control', 'id' => 'advanced-search', 'placeholder' => 'Tìm kiếm nâng cao']) ?>
+                <span class="input-group-btn">
+                <?= Html::submitButton('<i class="glyphicon glyphicon-search"></i>', ['class' => 'btn btn-primary']) ?>
+            </span>
+            </div><!-- /input-group -->
+            <?= Html::endForm() ?>
         </div>
-
     </div>
+    <?php Pjax::begin(); ?>
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+//            'id',
+            'user_name',
+            'first_name',
+            'last_name',
+            'email:email',
+            // 'avatar',
+            'gender',
+            'create_date',
+            'company',
+            'user_agent',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+    <?php Pjax::end(); ?>
 </div>
