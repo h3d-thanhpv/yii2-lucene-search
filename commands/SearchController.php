f3a2c0439d22bc9ad71b45f7ebf0cb548e8ae300
Phan Van Thanh <thanhpv@projectkit.net>
2016-10-12 10:50:30 +0700

    Improve search controller in console :droplet:

--- commands/SearchController.php
+++ commands/SearchController.php
@@ -4,14 +4,25 @@ namespace app\commands;
 
 use Yii;
 use yii\console\Controller;
+use himiklab\yii2\search\Search;
 
 class SearchController extends Controller
 {
+    /** @var Search $search */
+    private $search;
+
+    public function init()
+    {
+        parent::init();
+        $this->search = Yii::$app->search;
+    }
+
     public function actionIndex()
     {
-        ini_set('memory_limit', '-1');
-        /** @var \himiklab\yii2\search\Search $search */
-        $search = Yii::$app->search;
-        $search->index();
+        $this->search->index();
+    }
+    public function actionOptimize()
+    {
+        $this->search->optimize();
     }
 }
